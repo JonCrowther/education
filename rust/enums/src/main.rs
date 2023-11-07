93ad81cff67dbc8523a1e5714291d60026a6c5b4
Jonathan Crowther <jonathan.crowther@suse.com>
2023-11-07 10:52:17 -0500

    Chapter 6

---
+++ rust/enums/src/main.rs
@@ -0,0 +1,57 @@
+enum IpAddrKind {
+    V4(u8,u8,u8,u8),
+    V6(String),
+}
+
+fn route(ip_kind: IpAddrKind) {}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {}
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter (UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}", state);
+            25
+        }
+    }
+}
+
+fn main() {
+    let home = IpAddrKind::V4(127, 0, 0, 1);
+    let six = IpAddrKind::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+
+    println!("value {}", value_in_cents(Coin::Quarter(UsState::Alaska)));
+    m.call();
+}
